3e60d797788008dfa15ff16bdee5dbbf718bb7eb
nathaniel <nathaniel@playstudios.com>
2017-10-30 22:42:16 -0700

    adds routes, with stubs pointed to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/routes';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +10,14 @@ import { TwoButtonComponent } from './two-button/two-button.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ChatBoxComponent } from './chat-box/chat-box.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'settings', redirectTo: 'home' },
+    { path: 'dashboard', redirectTo: 'home' },
+    { path: '**', redirectTo: 'home' }
+]
 
 @NgModule({
   declarations: [
@@ -23,6 +32,10 @@ import { ChatBoxComponent } from './chat-box/chat-box.component';
     BrowserModule,
     FormsModule,
     HttpModule
+    RouterModule.forRoot(
+        routes,
+        { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
