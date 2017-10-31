c5631b2e90be9ba7802bc1b088cb84cf7678ec16
nathaniel <nathaniel@playstudios.com>
2017-10-30 22:35:45 -0700

    initial commit with components and sematnic-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TwoButtonComponent } from './two-button/two-button.component';
+import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TwoButtonComponent,
+    LoginComponent,
+    NavBarComponent,
+    ChatBoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/two-button/two-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-button',
+  templateUrl: './two-button.component.html',
+  styleUrls: ['./two-button.component.css']
+})
+export class TwoButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
