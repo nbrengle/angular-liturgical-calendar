369124e69cce0be6a5bbb78c992b7bf7307d9181
nathaniel <nathaniel@playstudios.com>
2017-11-19 14:50:51 -0800

    first pass day card

--- src/app/day-card/day-card.component.ts
+++ src/app/day-card/day-card.component.ts
@@ -1,11 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { CalendarDay } from '../models/calendar-day.model';
+import { Celebration } from '../models/celebration.model';
+
 
 @Component({
   selector: 'app-day-card',
-  templateUrl: './day-card.component.html',
-  styleUrls: ['./day-card.component.css']
+  template: `
+  <div class="ui card">
+    <div class="content">
+      Date: {{ calends.date }}
+      Season: {{ calends.season }}
+      Season Week: {{ calends.season_week }}
+      Weekday: {{ calends.weekday }}
+    </div>
+    <div
+      *ngFor="let celebration of calends.celebrations"
+      class="{{ celebration.colour }}"
+    >
+      {{ celebration.title }} : {{ celebration.rank }}, {{ celebration.rank_number }}
+    </div>
+  </div>
+  `
 })
 export class DayCardComponent implements OnInit {
+  @Input() calends: CalendarDay;
 
   constructor() { }
 
